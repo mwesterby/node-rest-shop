89891a78862bf2ae988b2b26e92234f3356cca07
Michael-Westerby <michael.westerby@ibm.com>
2019-06-05 13:47:09 +0100

    Managing orders with Mongoose

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,35 +1,122 @@
 const express = require('express');
 const router = express.Router(); // express router - ships with express - gives us cabibilities to handle different routes, endpoints with differet http verbs
+const mongoose = require('mongoose');
+
+const Order = require('../models/order');
+const Product = require('../models/product');
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Orders were fetched'
-    });
+    Order.find() // no arguments - gets all orders
+    .select('product quantity _id')
+    .exec()
+    .then(docs => {
+        res.status(200).json({
+            count: docs.length,
+            orders: docs.map(doc => {
+                return {
+                    id: doc._id,
+                    product: doc.product,
+                    quantity: doc.quantity,
+                    request: {
+                        type: 'GET',
+                        url: 'http://localhost:3000/orders/' + docs._id
+                    }
+                }
+            })
+        });
+    })
+    .catch(err => {
+        res.status(500).json({
+            error: err
+        });
+    }) 
 });
 
 router.post('/', (req, res, next) => {
-    const order = {
-        productId: req.body.productId,
-        quantity: req.body.quantity
-    };
-    res.status(201).json({
-        message: 'Order was created',
-        order: order
-    });
+    Product.findById(req.body.productId) // checks that the product passed actually exists
+        .then(product => {
+            if (!product) { // if product is null(ish) - i.e. the product does not exist
+                return res.status(404).json({
+                    message: 'Product not found'
+                });
+            }
+            const order = new Order({
+                _id: mongoose.Types.ObjectId(),
+                quantity: req.body.quantity,
+                product: req.body.productId
+            });
+            return order
+                .save() // dont neded .exec() after a .save()
+                
+        })
+        .then(result => {
+            console.log(result);
+            res.status(201).json({
+                message: 'Order stored',
+                createdOrder: {
+                    id: result._id,
+                    product: result.product,
+                    quantity: result.quantity
+                },
+                request: {
+                    type: 'GET',
+                    url: 'http://localhost:3000/orders/' + result._id
+                }
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
 });
 
 router.get('/:orderId', (req, res, next) => {
-    res.status(200).json({
-        message: 'Order details',
-        orderId: req.params.orderId
-    });
+    Order.findById(req.params.orderId)
+    .exec()
+    .then(order => {
+        if (!order) {
+            return res.status(404).json({
+                message: 'Order not found'
+            });
+        }
+        res.status(200).json({
+            order: order,
+            request: {
+                type: 'GET',
+                url: 'http://localhost:3000/orders'
+            }
+        });
+    })
+    .catch(err => {
+        res.status(500).json({
+            error: err
+        });
+    })
 });
 
 router.delete('/:orderId', (req, res, next) => {
-    res.status(200).json({
-        message: 'Order deleted',
-        orderId: req.params.orderId
-    });
+    Order.remove({ _id: req.params.orderId })
+    .exec()
+    .then(result => {
+        res.status(200).json({
+            message: 'Order deleted',
+            request: {
+                type: 'POST',
+                url: 'http://localhost:3000/orders',
+                body: {
+                    productId: 'ID',
+                    quantity: 'Number'
+                }
+            }
+        });
+    })
+    .catch(err => {
+        res.status(500).json({
+            error: err
+        });
+    })
 });
 
 module.exports = router; // means router can be exported and used by other files
